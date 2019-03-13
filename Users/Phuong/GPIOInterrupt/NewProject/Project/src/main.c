28ae8f17681a4cc88a2b624d26dd0ebd524e97c1
jason103nguyen <jason103nguyen@gmail.com>
2019-03-13 23:04:47 +0700

    phuong: nop bai tap interrupt

---
+++ Users/Phuong/GPIOInterrupt/NewProject/Project/src/main.c
@@ -0,0 +1,107 @@
+//Bai nay giu nut nhan led se sang, tha nut nhan led se tat
+
+#include "delay.h"
+#include "stm32f4xx.h"
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_gpio.h"
+
+/*---------------------------Interrupt Systick-------------------------------*/
+void SysTick_Handler()
+{
+	TimingDelay_Decrement();
+}
+
+void Configure_PE4(void) {
+    /* Set variables used */
+    GPIO_InitTypeDef GPIO_InitStruct;
+    EXTI_InitTypeDef EXTI_InitStruct;
+    NVIC_InitTypeDef NVIC_InitStruct;
+    
+    /* Enable clock for GPIOD */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+    /* Enable clock for SYSCFG */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+    
+    /* Set pin as input */
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOE, &GPIO_InitStruct);
+    
+    /* Tell system that you will use PD0 for EXTI_Line0 */
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource4);
+    
+    /* PD0 is connected to EXTI_Line0 */
+    EXTI_InitStruct.EXTI_Line = EXTI_Line4;
+    /* Enable interrupt */
+    EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+    /* Interrupt mode */
+    EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+    /* Triggers on rising and falling edge */
+    EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Falling;
+    /* Add to EXTI */
+    EXTI_Init(&EXTI_InitStruct);
+ 
+    /* Add IRQ vector to NVIC */
+    /* PD0 is connected to EXTI_Line0, which has EXTI0_IRQn vector */
+    NVIC_InitStruct.NVIC_IRQChannel = EXTI4_IRQn;
+    /* Set priority */
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+    /* Set sub priority */
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+    /* Enable interrupt */
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    /* Add to NVIC */
+    NVIC_Init(&NVIC_InitStruct);
+}
+
+void Configure_PA6_PA7(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+void blink(void)
+{
+	GPIO_ToggleBits(GPIOA,GPIO_Pin_7);
+	delay_ms(500);
+}
+
+void EXTI4_IRQHandler(void) {
+    /* Make sure that interrupt flag is set */
+    if (EXTI_GetITStatus(EXTI_Line4) != RESET) {
+        /* Do your stuff when PD0 is changed */
+			while(GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_4) == 0){
+				GPIO_ResetBits(GPIOA,GPIO_Pin_6);
+			}
+        /* Clear interrupt flag */
+        EXTI_ClearITPendingBit(EXTI_Line4);
+    }
+}
+
+int main(){
+	
+	/*--------------------------Initialize Clock------------------------------*/
+	SystemCoreClockUpdate();                      /* Get Core Clock Frequency   */
+  if (SysTick_Config(SystemCoreClock / 1000)) { /* SysTick 1 msec interrupts  */
+    while (1);                                  /* Capture error              */
+  }
+	
+	Configure_PA6_PA7();
+	Configure_PE4();
+	
+	while(1){
+		GPIO_SetBits(GPIOA,GPIO_Pin_6);
+		blink();
+	}
+}
