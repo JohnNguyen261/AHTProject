e0c793f03aadb1edf1dfdcda2d3299c229f3052f
jason103nguyen <jason103nguyen@gmail.com>
2019-03-13 21:01:18 +0700

    Phuong: day la bai nut nhan

---
+++ Users/Phuong/GPIO5/NewProject/Project/src/main.c
@@ -0,0 +1,59 @@
+//Bai nay giu nut nhan led se sang, tha nut nhan led se tat
+
+#include "delay.h"
+#include "stm32f4xx.h"
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_gpio.h"
+
+/*---------------------------Interrupt Systick-------------------------------*/
+void SysTick_Handler()
+{
+	TimingDelay_Decrement();
+}
+
+void Configure_PA6(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+void Configure_PE4(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE,ENABLE);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOE, &GPIO_InitStructure);
+}
+
+int main(){
+	
+	/*--------------------------Initialize Clock------------------------------*/
+	SystemCoreClockUpdate();                      /* Get Core Clock Frequency   */
+  if (SysTick_Config(SystemCoreClock / 1000)) { /* SysTick 1 msec interrupts  */
+    while (1);                                  /* Capture error              */
+  }
+	
+	Configure_PA6();
+	Configure_PE4();
+	
+	while(1){
+		if(GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_4) == 1){
+			GPIO_SetBits(GPIOA,GPIO_Pin_6);
+		}
+		else{
+			GPIO_ResetBits(GPIOA,GPIO_Pin_6);
+		}
+	}
+}
