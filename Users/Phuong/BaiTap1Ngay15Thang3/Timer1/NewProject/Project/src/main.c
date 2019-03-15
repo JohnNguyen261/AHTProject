8ff8f850477e7571ab98887146fb8e7e61f05646
jason103nguyen <jason103nguyen@gmail.com>
2019-03-16 00:03:16 +0700

    Phuong: nop bai tap 1 va 2

---
+++ Users/Phuong/BaiTap1Ngay15Thang3/Timer1/NewProject/Project/src/main.c
@@ -0,0 +1,233 @@
+//Bai nay giu nut nhan led se sang, tha nut nhan led se tat
+
+#include "delay.h"
+#include "stm32f4xx.h"
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_tim.h"
+
+int soLanNhan = 0;
+int thoiGianBatDau = 0;
+int thoiGianA = 0;
+int thoiGianB = 0;
+int chuongTrinhLed = 0;
+int thoiGianCu = 0;
+unsigned int miliGiay = 0;
+/*---------------------------Interrupt Systick-------------------------------*/
+void SysTick_Handler()
+{
+	TimingDelay_Decrement();
+}
+
+void init_Clock()
+{
+	// Resets the clock configuration to the default reset state
+	RCC_DeInit();
+
+	// Enable external crystal (HSE)
+	RCC_HSEConfig(RCC_HSE_ON);
+	// Wait until HSE ready to use or not
+	ErrorStatus errorStatus = RCC_WaitForHSEStartUp();
+
+	if (errorStatus == SUCCESS)
+	{
+		// Configure the PLL for 168MHz SysClk and 48MHz for USB OTG, SDIO
+		RCC_PLLConfig(RCC_PLLSource_HSE, 8, 336, 2, 7);
+		// Enable PLL
+		RCC_PLLCmd(ENABLE);
+		// Wait until main PLL clock ready
+		while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET);
+
+		// Set flash latency
+		FLASH_SetLatency(FLASH_Latency_5);
+
+		// AHB 168MHz
+		RCC_HCLKConfig(RCC_SYSCLK_Div1);
+		// APB1 42MHz
+		RCC_PCLK1Config(RCC_HCLK_Div4);
+		// APB2 84 MHz
+		RCC_PCLK2Config(RCC_HCLK_Div2);
+
+		// Set SysClk using PLL
+		RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+	}
+	else
+	{
+// Do something to indicate that error clock configuration
+	while (1);
+}
+
+SystemCoreClockUpdate();
+}
+
+void Configure_Timer2(void)
+{
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
+	
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	
+	TIM_TimeBaseInitStruct.TIM_Prescaler = 420;
+	TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInitStruct.TIM_Period = 200;
+	TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStruct);
+	//Config NVIC
+	TIM_ITConfig(TIM2,TIM_IT_Update,ENABLE);
+	TIM_Cmd(TIM2, ENABLE);
+	
+	NVIC_InitTypeDef NVIC_InitStruct;
+	NVIC_InitStruct.NVIC_IRQChannel = TIM2_IRQn;
+	/* Set priority */
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+	/* Set sub priority */
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+	/* Enable interrupt */
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	/* Add to NVIC */
+	NVIC_Init(&NVIC_InitStruct);
+	
+}
+
+void Configure_PA6(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_SetBits(GPIOA,GPIO_Pin_6);
+	GPIO_SetBits(GPIOA,GPIO_Pin_7);
+}
+
+void Configure_PE4(void) {
+    /* Set variables used */
+    GPIO_InitTypeDef GPIO_InitStruct;
+    EXTI_InitTypeDef EXTI_InitStruct;
+    NVIC_InitTypeDef NVIC_InitStruct;
+    
+    /* Enable clock for GPIOD */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+    /* Enable clock for SYSCFG */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+    
+    /* Set pin as input */
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOE, &GPIO_InitStruct);
+    
+    /* Tell system that you will use PD0 for EXTI_Line0 */
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource4);
+    
+    /* PD0 is connected to EXTI_Line0 */
+    EXTI_InitStruct.EXTI_Line = EXTI_Line4;
+    /* Enable interrupt */
+    EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+    /* Interrupt mode */
+    EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+    /* Triggers on rising and falling edge */
+    EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Falling;
+    /* Add to EXTI */
+    EXTI_Init(&EXTI_InitStruct);
+ 
+    /* Add IRQ vector to NVIC */
+    /* PD0 is connected to EXTI_Line0, which has EXTI0_IRQn vector */
+    NVIC_InitStruct.NVIC_IRQChannel = EXTI4_IRQn;
+    /* Set priority */
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x01;
+    /* Set sub priority */
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x10;
+    /* Enable interrupt */
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    /* Add to NVIC */
+    NVIC_Init(&NVIC_InitStruct);
+}
+
+void TIM2_IRQHandler(void) {
+    /* Make sure that interrupt flag is set */
+		if(TIM_GetITStatus(TIM2,TIM_IT_Update) != RESET){
+			miliGiay++;
+			TIM_ClearFlag(TIM2,TIM_FLAG_Update);
+		}
+}
+
+int xuLyNutNhan(void){
+	//Bat dau dem thoi gian
+	//Neu thoi gian cho chua het thi van doi nhan
+	//Neu da het thi setup trang thai
+	
+	int ret_val = 0;
+	thoiGianCu = miliGiay;
+	while(miliGiay - thoiGianCu < 2000){
+		if(GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_4) == 0){
+			while(GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_4) == 0){}
+			soLanNhan++;
+			thoiGianCu = miliGiay;
+		}
+	}
+	if(soLanNhan == 2){
+		ret_val = 1;
+	}
+	else if(soLanNhan >=3 && soLanNhan <=5){
+		ret_val = 2;
+	}
+	else if(soLanNhan >5){
+		ret_val = 3;
+	}
+	return ret_val;
+}
+
+void EXTI4_IRQHandler(void) {
+    /* Make sure that interrupt flag is set */
+    if (EXTI_GetITStatus(EXTI_Line4) != RESET) {
+        /* Do your stuff when PD0 is changed */
+				chuongTrinhLed = xuLyNutNhan();
+        /* Clear interrupt flag */
+        EXTI_ClearITPendingBit(EXTI_Line4);
+    }
+}
+
+int main(){
+	init_Clock();
+  if (SysTick_Config(SystemCoreClock / 1000)) { 
+    while (1);                               
+  }
+	
+	Configure_Timer2();
+	Configure_PA6();
+	Configure_PE4();
+	
+	
+	while(1){
+		
+		if(chuongTrinhLed == 1){
+			GPIO_SetBits(GPIOA,GPIO_Pin_7);
+			GPIO_SetBits(GPIOA,GPIO_Pin_6);
+			delay_ms(500);
+			GPIO_ResetBits(GPIOA,GPIO_Pin_6);
+			delay_ms(500);
+		}
+		else if(chuongTrinhLed == 2){
+			GPIO_SetBits(GPIOA,GPIO_Pin_6);
+			GPIO_SetBits(GPIOA,GPIO_Pin_7);
+			delay_ms(500);
+			GPIO_ResetBits(GPIOA,GPIO_Pin_7);
+			delay_ms(500);
+		}
+		else if(chuongTrinhLed == 3){
+			GPIO_SetBits(GPIOA,GPIO_Pin_6);
+			GPIO_SetBits(GPIOA,GPIO_Pin_7);
+			delay_ms(500);
+			GPIO_ResetBits(GPIOA,GPIO_Pin_6);
+			GPIO_ResetBits(GPIOA,GPIO_Pin_7);
+			delay_ms(500);
+		}
+	}
+}
